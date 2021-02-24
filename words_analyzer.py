9c7a0f8359eba9a8163b6872fa6296c1972152c3
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-24 10:38:59 +0300

    handle POST and GET methods

--- words_analyzer.py
+++ words_analyzer.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/analyze")
+
+@app.route("/analyze", methods=["GET", "POST"])
 def analyze():
-    return "hello"
+    if request.method == "POST":
+        json_data = request.get_json()  # raise and return "bad request" if non-valid json
+        return json_data
+    else:
+        return """
+        <p>Send POST request with JSON string to parse, e.g.</p>
+        <code>curl --header "Content-Type: application/json" \
+              --request POST --data '{"text":"hello 2 times  "}' \
+              https://mysuperawesomeapi.com/analyze</code>
+        """
