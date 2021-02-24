e0561c958919466ef82d41001dec85c1b30eb1b6
Ivan Nikiforov <nikiforr@gmail.com>
2021-02-24 10:59:20 +0300

    parse json data from POST

--- words_analyzer.py
+++ words_analyzer.py
@@ -1,13 +1,32 @@
+import collections
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
+def count_spaces(s: str) -> int:
+    return s.count(" ")
+
+def count_words(s: str) -> int:
+    return len(s.split())
+
+def count_chars(s: str):
+    return collections.Counter(s)
+
 @app.route("/analyze", methods=["GET", "POST"])
 def analyze():
     if request.method == "POST":
         json_data = request.get_json()  # raise and return "bad request" if non-valid json
-        return json_data
+        if "text" not in json_data:
+            abort(400)
+        return {
+            "textLength": {
+                "withSpaces": len(json_data["text"]),
+                "withoutSpaces": len(json_data["text"]) - count_spaces(json_data["text"]),
+            },
+            "wordCount": count_words(json_data["text"]),
+            "characterCount": count_chars(json_data["text"]),
+        }
     else:
         return """
         <p>Send POST request with JSON string to parse, e.g.</p>
